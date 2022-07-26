439515248d8c3d9682b9c16d026716dc84fa3bd3
stacyvana <ssvana83@yahoo.com>
2022-07-26 09:49:59 -0400

    commit tree 569a9ab986b78a74e4df90f211c1d8a1c9aa05c1

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Code your solutions in this file
+// for (let age = 30; age < 40; age++) {
+//     console.log(`I'm ${age} years old. Happy birthday to me!`);
+//    // debugger;
+// }
+
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//     for (let i = 0; i < gifts.length; i++) {
+//         console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//         //debugger;
+//     }
+
+//     return gifts;
+// }
+
+// wrapGifts(gifts);
+
+const cards = ["Stacy", "Chloe", "Aliza"];
+
+function writeCards(cards) {
+    const messages = []
+    for (let i = 0; i < cards.length; i++) {
+        messages.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`)
+        //debugger;
+    }
+
+    return messages;
+}
+
+writeCards(cards);
+
+function countDown(down) {
+    let countDown = 10;
+    while (countDown >= 0){
+console.log(countDown--)
+    };
+}
+
+
+// const name = ["Natalie", "Mia", "Sophia", "Ava"];
+
+// function writeCards(name) {
+//     for (let i = 0, i < )
+
+// }
